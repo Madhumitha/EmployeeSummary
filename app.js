e88350550324f1ff117e319263cbe22748eb445a
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-03 16:33:06 -0600

    Add prompt for user

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 const inquirer = require('inquirer');
+
+// Prompt an user for information such as email id and specific information 
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type : 'input',
+            name : 'emailID',
+            message: 'Email id : '
+        },
+        {
+            type: 'list',
+            name : 'categories',
+            message: 'Choose whether Intern or Engineer',
+            choices : ['Engineer', 'Intern', 'Manager'],
+            default: 'Manager'
+        },
+        {
+            type: 'input',
+            name: 'schoolName',
+            message: 'Interns school name : ',
+            when : (answers) => answers.categories === 'Intern'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Github User name : ',
+            when : (answers) => answers.categories === 'Engineer'
+        }
+    ])
+}
+
+async function asyncCall() {
+    const answers = await promptUser();
+}
+
+// Function Calls
+
+asyncCall();
