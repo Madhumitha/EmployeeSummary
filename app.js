deb743a525b164125f72160bb3d051e5afa51ec5
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-05 16:55:15 -0600

    Fix promptUser() functionalities

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const inquirer = require('inquirer');
 
-// Prompt an user for information such as email id and specific information 
+// Prompt an user for information such as email id and specific information
 
 function promptUser() {
     return inquirer.prompt([
         {
-            type : 'input',
-            name : 'emailID',
-            message: 'Email id : '
-        },
-        {
             type: 'list',
             name : 'categories',
-            message: 'Choose whether Intern or Engineer',
-            choices : ['Engineer', 'Intern', 'Manager'],
-            default: 'Manager'
+            message: 'Choose whether Manager, Intern or Engineer',
+            choices : ['Engineer', 'Intern', 'Manager']
+        }, 
+        {
+            type: 'input',
+            name : 'officeNo.',
+            message: 'Managers office number : ',
+            when: (answers) => answers.categories === 'Manager'
         },
         {
             type: 'input',
@@ -27,14 +27,37 @@ function promptUser() {
             name: 'github',
             message: 'Github User name : ',
             when : (answers) => answers.categories === 'Engineer'
-        }
-    ])
+        },
+        {
+            type: 'input',
+            name: 'email-id',
+            message: 'Email-id : '
+        },
+        ])
 }
+
+// Generate a HTML page
 
 async function asyncCall() {
-    const answers = await promptUser();
+    try {
+        const answers = await promptUser();
+        let reply = await inquirer.prompt(
+            {
+                type: 'list',
+                name: 'moreInput',
+                message: 'Do you want to add members for EmployeeSummary : ',
+                choices: ['Yes', 'No']
+            })
+        
+        if(reply.moreInput === 'Yes') {
+            asyncCall();
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
-// Function Calls
+// Function Calls 
 
 asyncCall();
